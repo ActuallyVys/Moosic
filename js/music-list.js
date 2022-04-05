96bdef0372a8ea4a6ff8c5c94dfbea8c7ab89302
vys <92237104+ActuallyVys@users.noreply.github.com>
2022-04-05 14:20:45 +0000

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,47 @@
+// To add more songs, just copy the following code and paste inside the array
+
+//   {
+//     name: "Song Name",
+//     artist: "Artist's Name",
+//     img: "Image_Name.jpg - Must be in .jpg format, unless configured otherwise.",
+//     src: "Music_Name.mp3 - Must be in .mp3 format!"
+//   }
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+];
